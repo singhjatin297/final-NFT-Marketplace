d86e81a9c809293c1dc69cf22c759bbb0955a348
singhjatin297 <78216862+singhjatin297@users.noreply.github.com>
2023-05-23 06:15:57 +0530

    Update hardhat.config.js
    deploy polygon testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "YOUR RPC URL";
-const NEXT_PUBLIC_PRIVATE_KEY = "PRIVATE KEY";
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "Yhttps://polygon-mumbai.g.alchemy.com/v2/a7rfBC-qsWN3K8p0sSE7dnxr1SUu1-wP";
+const NEXT_PUBLIC_PRIVATE_KEY = "4fd39a7d6c18d067187d4b5cac66e81bae3abf9218822c397946c945ed491506";
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
